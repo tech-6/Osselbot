a702361cc5d2dc8e25c5fa6eec15e2462f685c67
Alex Brow <browa84@gmail.com>
2020-02-19 20:39:47 -0500

    perms v2

--- src/index.js
+++ src/index.js
@@ -72,14 +72,14 @@ client.on("message", async message => {
   	if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use.
     // To get the "message" itself we join the `args` back into a string with spaces:
-    	const sayMessage = args.join(" ");
-	if(sayMessage.includes('@'))
-		return message.reply("\nStop pinging yourself \nStop pinging yourself");
-	else if (!message.member.roles.some(r=>["Admin", "Mods","Member of the Order of the b l u e","Botmeister","Ally of the Order"].includes(r.name)) ) {
-		// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-		  message.delete().catch(O_o=>{});
-		// And we get the bot to say the thing:
-		   return message.channel.send(sayMessage);
+    		const sayMessage = args.join(" ");
+		if(sayMessage.includes('@'))
+			return message.reply("\nStop pinging yourself \nStop pinging yourself");
+		if (!message.member.roles.some(r=>["Admin", "Mods","Member of the Order of the b l u e","Botmeister","Ally of the Order"].includes(r.name)) ) {
+			// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+			message.delete().catch(O_o=>{});
+			// And we get the bot to say the thing:
+			return message.channel.send(sayMessage);
 		}
 		else {
 			return message.channel.send("Im sorry but we cant have nice things...")
