08a190e6d940a8963be25293dcfc6e9172dc1e05
Alex Brow <browa84@gmail.com>
2020-02-21 23:06:37 -0500

    AAAAAAAAAAAAAA

--- src/index.js
+++ src/index.js
@@ -18,19 +18,19 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`${guild.memberCount} of you horrible people`,{ type: 'LISTENING' });
+  client.user.setActivity(`${client.users.size} of you horrible people`,{ type: 'LISTENING' });
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`${guild.memberCount} of you horrible people`,{ type: 'LISTENING' });
+  client.user.setActivity(`${client.users.size} of you horrible people`,{ type: 'LISTENING' });
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`${guild.memberCount} of you horrible people`,{ type: 'LISTENING' });
+  client.user.setActivity(`${client.users.size} of you horrible people`,{ type: 'LISTENING' });
 });
 //osseley stop
 
