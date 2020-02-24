5343bc9331ca755a5d15ec050c74a539c16fdec2
Alex Brow <browa84@gmail.com>
2020-02-23 22:17:41 -0500

    brub

--- src/index.js
+++ src/index.js
@@ -288,7 +288,7 @@ client.on("message", async message => {
 		'<aviadarius> i forgot',
 		'<N29UB> Asia is a continent'
 		]
-		let number = Math.floor(Math.random() * (max - min) + min)]) //The maximum is exclusive and the minimum is inclusive
+		let number = Math.floor(Math.random() * (max - min) + min) //The maximum is exclusive and the minimum is inclusive
   		return message.channerl.send(quotes[number])
 	}
 
