13c2d946bba4dc4735341724c6f832e8dd36844d
Alex Brow <browa84@gmail.com>
2020-03-22 23:50:06 -0400

    ahah

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ShardingManager } = require('discord.js');
 const manager = new ShardingManager('./bot.js',config.token);
+const config = require("./config.json");
 
 manager.spawn();
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
