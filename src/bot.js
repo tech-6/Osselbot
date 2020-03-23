678a7cbe65b672b86bb14b03dbd535f504f671a1
Alex Brow <browa84@gmail.com>
2020-03-23 00:10:09 -0400

    oaakd'lm

--- src/bot.js
+++ src/bot.js
@@ -14,7 +14,7 @@ const config = require("./config.json");
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.cache.cache.size} users, in ${client.channels.cache.cache.size} channels of ${client.guilds.cache.cache.size} guilds.`);
+  console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
   client.user.setActivity(`${client.users.cache.size} of you horrible people`,{ type: 'LISTENING' });
