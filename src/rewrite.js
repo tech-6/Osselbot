e30798fb3028e7d1fc7042733517a1a9fca43cb0
Alex <alex@2a03.party>
2020-05-15 01:50:27 -0500

    nice nice nice

--- src/rewrite.js
+++ src/rewrite.js
@@ -64,8 +64,92 @@ client.on("message", async message => {
 		return message.channel.send(embed);
 	};
 	//////////////////////////////////////////////////////////////////////////////
-	
+	if(command === "ping") {
+	// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+		const m = await message.channel.send("Ping?");
+		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms`);
+	};
+	//////////////////////////////////////////////////////////////////////////////
+	//HOW QUOTEABLE
+	if(command === "psych") {
+		return message.channel.send("Rules for finding a psychopath: \n1. Favorite color is orange \n2. Likes the left burners, worse if its top left\n3. Calls pizza sauce/tomato sauce gravy\n4. Doesnt like salad\n5. Likes country music\n6. Makes hot chocolate with water\n7. Likes black licorice")
+	};
 
+	if(command === "ask") {
+		return message.channel.send("Dont Ask, Just ask!\n https://iki.fi/sol/dontask.html")
+	};
+
+	if(command === "simp") {
+		if(!message.member.roles.cache.some(r=>["Admin", "Moderator","Member of the Order of the b l u e","Botmeister"].includes(r.name)) )
+	return message.reply("Sorry, you don't have permissions to use this!");
+		let embed = new Discord.MessageEmbed()
+			.setTitle("SIMP Alert")
+			.setURL("https://www.youtube.com/watch?v=c3m4Q07TkMk")
+			.setColor(0x195080)
+			.setDescription("__**ALERT**__ SIMP detected!")
+			.setImage("https://vignette.wikia.nocookie.net/disney/images/6/64/Kronk_.jpg/revision/latest?cb=20160720194635")
+			.setTimestamp()
+		message.channel.send({embed})
+	};
+
+	if(command === "squad") {
+		min = Math.ceil(0);
+  		max = Math.floor(11);
+  		let rate = Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+		return message.reply(`The squad rates this ${rate} out of 10`);
+	};
+
+	if(command === "magic") {
+		return message.channel.send("Do you believe in magic in a young girl\'s heart\nHow the music can free her, whenever it starts\nAnd it\'s magic, if the music is groovy\nIt makes you feel happy like an old-time movie\nI\'ll tell you about the magic, and it\'ll free your soul\nBut it\'s like trying to tell a stranger bout \'rock and roll\'");
+	};
+
+	if(command === 'help') {
+		return message.reply('https://technicolor.2a03.party/bot/');
+	};
+
+	if (command === "fix") {
+		if(message.member.roles.cache.some(r=>["Botmeister"].includes(r.name))) {
+		return message.channel.send("I guess it\'s my fault will fix.");
+		};
+		return;
+	};
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////DEFCON//////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+	if(command === "defcon") {
+		//5 levels till ban
+		if(!message.member.roles.cache.some(r=>["Admin","Member of the Order of the b l u e","Botmeister"].includes(r.name)) )
+		return message.reply("Sorry, you don't have permissions to use this!");
+		else {
+			let member = message.mentions.members.first();
+			var role = "";
+			switch(parseInt(args[1],10)) {
+				case 5:
+					var role = message.guild.roles.cache.find(role => role.name === `DEFCON 5`);
+					member.roles.add(role);
+				break;
+				case 4:
+					var role = message.guild.roles.cache.find(role => role.name === `DEFCON 4`);
+					member.roles.add(role);
+						break;
+					case 3:
+					var role = message.guild.roles.cache.find(role => role.name === `DEFCON 3`);
+						member.roles.add(role);
+				break;
+					case 2:
+					var role = message.guild.roles.cache.find(role => role.name === `DEFCON 2`);
+					member.roles.add(role);
+						break;
+					case 1:
+					var role = message.guild.roles.cache.find(role => role.name === `DEFCON 1`);
+					member.roles.add(role);
+						break;
+				default:
+					return message.reply(`DEFCON not set is ${args[1]} a number between 1-5?`)
+			};
+			return message.reply(`\n**DEFCON** level set to DEFCON ${args[1]}\nGod Bless their souls`)
+		};
+	};
 });
 
 //Logging in the bot
