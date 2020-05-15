b38c21b9e00bcde67e208b539b306365f337f269
Alex <alex@2a03.party>
2020-05-15 12:33:20 -0500

    agagagag

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+#logs in as Alex
+cat ~/.GHTOKEN | docker login https://docker.pkg.github.com -u technicolor-creamsicle --password-stdin
 #Builds the package
 docker build --tag osselbot .
 #pushes to github
-docker tag osselbot:latest docker.pkg.github.com/techniclor-creamsicle/osselbot/osselbot:$0
-docker push docker.pkg.github.com/techniclor-creamsicle/osselbot/osselbot:$0
+docker tag osselbot:latest docker.pkg.github.com/techniclor-creamsicle/osselbot/osselbot:$1
+docker push docker.pkg.github.com/techniclor-creamsicle/osselbot/osselbot:$1
 
-echo 'build must have passed using version $0'
+echo build may have passed using version: $1
