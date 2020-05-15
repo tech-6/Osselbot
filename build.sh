3f9532cf561c08a7d01e41e78dcef4555a092451
Alex <alex@2a03.party>
2020-05-15 15:44:19 -0500

    removing docker

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
 #logs in as Alex
-cat ~/.GHTOKEN | docker login https://docker.pkg.github.com -u technicolor-creamsicle --password-stdin
-#Builds the package
-docker build --tag osselbot .
-#pushes to github
-docker tag osselbot:latest docker.pkg.github.com/techniclor-creamsicle/osselbot/osselbot:$1
-docker push docker.pkg.github.com/techniclor-creamsicle/osselbot/osselbot:$1
-
-echo build may have passed using version: $1
