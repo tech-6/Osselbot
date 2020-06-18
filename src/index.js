136b88a77dccd4b56f1216aec2392c5a0138ebc3
alex <alex@2a03.party>
2020-06-18 02:06:34 -0500

    poggers

--- src/index.js
+++ src/index.js
@@ -49,9 +49,9 @@ client.on("message", async message => {
 	if(message.author.bot) return;
 	if(message.content.indexOf(config.prefix) !== 0) return;
 	// Here we separate our "command" name, and our "arguments" for the command.
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
+  	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  	// command = say
+  	// args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 	//////////////////////////////////////////////////////////////////////////////
@@ -237,4 +237,4 @@ if(process.argv.slice(2).includes("--TEST")) {
 };
 
 //Logging in the bot
-client.login(config.token)
+client.login(config.token);
